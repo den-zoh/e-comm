dc1ed2d27e02693a52227937d76ee80a356b84f5
Dennis Ndungu <dmwika@strathmore.edu>
2022-01-05 19:31:07 +0300

    added styling to the page

--- First React/index.js
+++ First React/index.js
@@ -10,23 +10,61 @@
 //                         <p>Welcome to React</p>
 //                     </nav>);
 
-function GetWebPage() {
+function Header() {
     return(
         <div>
-            <img src="aquana-pic.png" width="40px" alt="sorry"/>
-            <h1>Welcome to this Page</h1>
-            <p>This is my first react app. I love it so much because:</p>
-            <ul>
-                <li>It is a hirable skill</li>
-                <li>It is well maintained</li>
-                <li>It's error handling is good.</li>
-                <li>It is a great skill to learn</li>
-            </ul>
+            <header>
+                <nav className="nav">
+                    <img className="nav-logo" src="aquana-pic.png" alt="sorry" />
+                    <ul className="nav-items">
+                        <li>Pricing</li>
+                        <li>About</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </nav>
+            </header>
         </div>
     )
 }
 
-ReactDOM.render(<GetWebPage/>, document.getElementById("root"))
+function MainContent(){
+    return(
+        <div className="content-body">
+        <h1>Welcome to this Page</h1>
+            <p>This is the Best Javascript Library</p>
+            <ol>
+                <li>It is a hirable skill</li>
+                <li>It is well maintained</li>
+                <li>It's error handling is good.</li>
+                <li>It is a great skill to learn</li>
+            </ol>
+        </div>
+    )
+}
+
+function Footer() {
+    return(
+
+        <div>
+            <footer className="footer-text">
+               <small> © 2022 Mespa Developers. All Rights Reserved.</small>
+            </footer>
+        </div>
+    )
+}
+
+function Page(){
+    return(
+        <div>
+        <Header/>
+        <MainContent />
+        <Footer/>
+        </div>
+    )
+}
+
+ReactDOM.render(<Page />,
+    document.getElementById("root"))
 
 
 
