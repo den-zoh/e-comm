dda78c662c491a7a00671621046ecb66db3c3b48
Dennis Ndungu <dmwika@strathmore.edu>
2022-01-11 00:00:49 +0300

    added new application (sorted the issue)

---
+++ int-react/src/header.js
@@ -0,0 +1,17 @@
+function Header() {
+    return(
+        <div>
+            <header>
+                <nav className="nav">
+                    <img className="nav-logo" src="aquana-pic.png" alt="sorry" />
+                    <ul className="nav-items">
+                        <li>Pricing</li>
+                        <li>About</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    )
+}
+export default Header;
